4471bbb19475a443ac7d4a931819300070df87c5
susunekiyotaka-eng <susunekiyotaka@gmail.com>
2026-02-13 00:17:36 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+document.querySelectorAll(".curso").forEach(curso => {
 
+  curso.addEventListener("click", function(){
+
+    if(this.classList.contains("bloqueado")){
+      alert("Debes aprobar su requisito primero 💗");
+      return;
+    }
+
+    this.classList.add("aprobado");
+
+    desbloquearCursos();
+  });
+
+});
+
+function desbloquearCursos(){
+
+  document.querySelectorAll(".curso.bloqueado").forEach(curso => {
+
+    let prereqs = curso.getAttribute("data-prereq");
+
+    if(!prereqs) return;
+
+    let requisitos = prereqs.split(",");
+
+    let todosAprobados = requisitos.every(req =>
+      document.querySelector(`[data-id='${req.trim()}']`)
+      ?.classList.contains("aprobado")
+    );
+
+    if(todosAprobados){
+      curso.classList.remove("bloqueado");
+    }
+
+  });
+
+}
